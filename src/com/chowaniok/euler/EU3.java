69bdf0cff302be9e6a1946d0ee877889d9ceb011
Marek Chowaniok <marek.chowaniok@gmail.com>
2011-09-03 19:22:56 +0200

    added solution for #3

---
+++ src/com/chowaniok/euler/EU3.java
@@ -0,0 +1,32 @@
+package com.chowaniok.euler;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+import cern.jet.math.Functions;
+
+public class EU3 {
+
+	public long solve(long x) {
+		long a = 2;
+		while (x > 1) {
+			if ((x % a) == 0) {
+				x = x / a;
+			} else {
+				a++;
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		StopWatch timer = new StopWatch();
+		timer.start();
+		long result  = new EU3().solve(600851475143L);
+		System.out.println("result: " + result + " time: "	+ timer.getNanoTime());
+	}
+
+}
